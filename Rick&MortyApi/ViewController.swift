c0947d0fa3f2f57e9586cb24e15a65d44ab5b3c7
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-11 14:14:30 +0300

    Initial Commit

---
+++ Rick&MortyApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&MortyApi
+//
+//  Created by Ömer Faruk Kılıçaslan on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
